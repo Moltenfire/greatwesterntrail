64361305f4e73aa3097b39b8f63526f90668489a
Jonathan Davies <moltenfires@googlemail.com>
2025-02-05 20:51:16 +0000

    Change rng

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
-const prng = splitmix32((Math.random()*2**32)>>>0);
+let rng = new RNG(0);
 const version = 0;
 
 function onload() {
-    let yourDate = new Date()
-    Math.seedrandom(yourDate.toISOString().split('T')[0]);
+    let date = new Date();
+    let y = `${date.getFullYear()}`
+    let m = `${date.getMonth()+1}`.padStart(2, "0")
+    let d = `${date.getDay()+1}`.padStart(2, "0")
+    let seed = parseInt(y + m + d)
+    console.log(date)
+    console.log(y, m, d, seed)
+    rng = new RNG(seed);
     newgame();
 }
 
@@ -23,19 +29,19 @@ function getRandomGameState() {
         bonus_cards: [],
     }
     for (let i = 1; i <= 10; i++) {
-        gamestate.buildings.push(prng() < 0.5 ? 0 : 1);
+        gamestate.buildings.push(rng.nextRange(0, 2));
     }
 
     let neutral_buildings = ["A", "B", "C", "D", "E", "F", "G", "H"];
     for (let i = neutral_buildings.length - 1; i > 0; i--) {
-        let j = Math.floor(prng() * (i + 1));
+        let j = rng.nextRange(0, i+1);
         [neutral_buildings[i], neutral_buildings[j]] = [neutral_buildings[j], neutral_buildings[i]];
     }
     gamestate.neutral_buildings = neutral_buildings;
 
     let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for (let i = 0; i < 4; i++) {
-        let index = Math.floor(prng() * numbers.length);
+        let index = rng.nextIndex(numbers);
         let n = numbers[index];
         gamestate.bonus_cards.push(n);
         numbers.splice(index, 1);
@@ -97,14 +103,28 @@ function stateValue(state) {
     return value;
 }
 
-function splitmix32(a) {
-    return function() {
-        a |= 0;
-        a = a + 0x9e3779b9 | 0;
-        let t = a ^ a >>> 16;
-        t = Math.imul(t, 0x21f0aaad);
-        t = t ^ t >>> 15;
-        t = Math.imul(t, 0x735a2d97);
-        return ((t = t ^ t >>> 15) >>> 0) / 4294967296;
-    }
+function RNG(seed) {
+    // LCG using GCC's constants
+    this.m = 0x80000000; // 2**31;
+    this.a = 1103515245;
+    this.c = 12345;
+    this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+}
+RNG.prototype.nextInt = function() {
+    this.state = (this.a * this.state + this.c) % this.m;
+    return this.state;
+}
+RNG.prototype.nextFloat = function() {
+    // returns in range [0,1]
+    return this.nextInt() / (this.m - 1);
+}
+RNG.prototype.nextRange = function(start, end) {
+    // returns in range [start, end): including start, excluding end
+    // can't modulu nextInt because of weak randomness in lower bits
+    var rangeSize = end - start;
+    var randomUnder1 = this.nextInt() / this.m;
+    return start + Math.floor(randomUnder1 * rangeSize);
+}
+RNG.prototype.nextIndex = function(array) {
+    return this.nextRange(0, array.length);
 }
