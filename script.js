6fd01f41cad1e143eaa8365f340ebdefc625fa1b
Jonathan Davies <moltenfires@googlemail.com>
2025-07-16 09:52:11 +0100

    Add GWT

--- script.js
+++ script.js
@@ -1,41 +1,86 @@
+import { RNG } from './rng.js';
 
 let date = new Date();
-let seed = parseInt(date.toISOString().split("T")[0].replaceAll("-", ""))
+let seed = parseInt(date.toISOString().split("T")[0].replaceAll("-", ""));
 let rng = new RNG(seed);
 let newgames = 0;
 const version = 0;
 
-function onload() {
-    console.log(`seed ${seed}`)
-    rng = new RNG(seed);
-    newgame();
-}
-
-function newgame() {
-    state = getRandomGameState();
-    const div = document.getElementById("gwt-nz");
-    div.querySelector('.buildings').innerText = buildingString(state);
-    div.querySelector('.neutral_buildings').innerText = neutralBuildingString(state);
-    div.querySelector('.bonus_cards').innerText = bonusCardsString(state);
-    div.querySelector('.state_value').innerText = stateValue(state);
-
+export function newgame() {
     newgames++;
-    console.log(`Game ${newgames}: ${buildingString(state)} - ${neutralBuildingString(state)} - ${bonusCardsString(state)} - ${stateValue(state)}`);
+    updateGWTNZ();
+    updateGWT();
 }
 
-function getRandomGameState() {
+function updateGWT() {
+    let state = getRandomGameStateGWT();
+    document.getElementById('gwt-buildings').innerText = buildingString(state);
+    document.getElementById('gwt-neutral').innerText = neutralBuildingString(state);
+    document.getElementById('gwt-state').innerText = state.stateValue();
+}
+
+function updateGWTNZ() {
+    let state = getRandomGameStateGWTNZ();
+    document.getElementById('gwt-nz-buildings').innerText = buildingString(state);
+    document.getElementById('gwt-nz-neutral').innerText = neutralBuildingString(state);
+    document.getElementById('gwt-nz-bonus-cards').innerText = bonusCardsString(state);
+    document.getElementById('gwt-nz-state').innerText = state.stateValue();
+    console.log(`Game ${newgames}: ${buildingString(state)} - ${neutralBuildingString(state)} - ${bonusCardsString(state)} - ${state.stateValue()}`);
+}
+
+function getRandomGameStateGWT() {
+    let gamestate = {
+        buildings: [],
+        neutral_buildings: [],
+    };
+
+    for (let i = 1; i <= 12; i++) {
+        gamestate.buildings.push(rng.nextRange(0, 2));
+    }
+
+    let neutral_buildings = ["A", "B", "C", "D", "E", "F", "G"];
+    for (let i = neutral_buildings.length - 1; i > 0; i--) {
+        let j = rng.nextRange(0, i + 1);
+        [neutral_buildings[i], neutral_buildings[j]] = [neutral_buildings[j], neutral_buildings[i]];
+    }
+    gamestate.neutral_buildings = neutral_buildings;
+
+    gamestate.stateValue = function() {
+        let value = "";
+
+        let neutral_buildings_value = this.neutral_buildings.slice(0, -1).join('');
+        value += neutral_buildings_value + " ";
+
+        let buildings_value = 0;
+        for (let i = 0; i < this.buildings.length; i++) {
+            if (this.buildings[i]) {
+                buildings_value |= 1 << i;
+            }
+        }
+
+        value += formatNumberAsHex(buildings_value, 3) + " ";
+        value += formatNumberAsHex(version);
+
+        return value;
+    };
+
+    return gamestate;
+}
+
+function getRandomGameStateGWTNZ() {
     let gamestate = {
         buildings: [],
         neutral_buildings: [],
         bonus_cards: [],
-    }
+    };
+
     for (let i = 1; i <= 10; i++) {
         gamestate.buildings.push(rng.nextRange(0, 2));
     }
 
     let neutral_buildings = ["A", "B", "C", "D", "E", "F", "G", "H"];
     for (let i = neutral_buildings.length - 1; i > 0; i--) {
-        let j = rng.nextRange(0, i+1);
+        let j = rng.nextRange(0, i + 1);
         [neutral_buildings[i], neutral_buildings[j]] = [neutral_buildings[j], neutral_buildings[i]];
     }
     gamestate.neutral_buildings = neutral_buildings;
@@ -49,6 +94,30 @@ function getRandomGameState() {
     }
     gamestate.bonus_cards = gamestate.bonus_cards.sort((a, b) => a - b);
 
+    gamestate.stateValue = function() {
+        let value = "";
+
+        let neutral_buildings_value = this.neutral_buildings.slice(0, -1).join('');
+        value += neutral_buildings_value + " ";
+
+        let bonus_cards_value = 0;
+        for (let i = 0; i < this.bonus_cards.length; i++) {
+            bonus_cards_value |= 1 << this.bonus_cards[i];
+        }
+
+        let buildings_value = 0;
+        for (let i = 0; i < this.buildings.length; i++) {
+            if (this.buildings[i]) {
+                buildings_value |= 1 << i;
+            }
+        }
+
+        value += formatNumberAsHex(buildings_value + (bonus_cards_value << 10), 5) + " ";
+        value += formatNumberAsHex(version);
+
+        return value;
+    };
+
     return gamestate;
 }
 
@@ -56,7 +125,7 @@ function buildingString(state) {
     let results = [];
     for (let i = 0; i < state.buildings.length; i++) {
         let side = state.buildings[i] == 0 ? 'A' : 'B';
-        results.push(`${i+1}${side}`);
+        results.push(`${i + 1}${side}`);
     }
     return results.join(' ');
 }
@@ -66,66 +135,13 @@ function neutralBuildingString(state) {
 }
 
 function bonusCardsString(state) {
-    let results = [];
-    for (let i = 0; i < state.bonus_cards.length; i++) {
-        results.push(state.bonus_cards[i]+1);
-    }
-    return results.join(' ');
+    return state.bonus_cards.map(n => n + 1).join(' ');
 }
 
 function formatNumberAsHex(num, length) {
     return num.toString(16).padStart(length, '0').toUpperCase();
 }
 
-function stateValue(state) {
-    let value = "";
-
-    neutral_buildings_value = "";
-    for (let i = 0; i < state.neutral_buildings.length-1; i++) {
-        neutral_buildings_value += state.neutral_buildings[i];
-    }
-    value += neutral_buildings_value + " ";
-
-    bonus_cards_value = 0;
-    for (let i = 0; i < state.bonus_cards.length; i++) {
-        bonus_cards_value |= 1 << state.bonus_cards[i];
-    }
-    
-    buildings_value = 0;
-    for (let i = 0; i < state.buildings.length; i++) {
-        if (state.buildings[i]) {
-            buildings_value |= 1 << i;
-        }
-    }
-    value += formatNumberAsHex(buildings_value + (bonus_cards_value << 10), 5) + " "
-
-    value += formatNumberAsHex(version);
-
-    return value;
-}
-
-function RNG(seed) {
-    // LCG using GCC's constants
-    this.m = 0x80000000; // 2**31;
-    this.a = 1103515245;
-    this.c = 12345;
-    this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
-}
-RNG.prototype.nextInt = function() {
-    this.state = (this.a * this.state + this.c) % this.m;
-    return this.state;
-}
-RNG.prototype.nextFloat = function() {
-    // returns in range [0,1]
-    return this.nextInt() / (this.m - 1);
-}
-RNG.prototype.nextRange = function(start, end) {
-    // returns in range [start, end): including start, excluding end
-    // can't modulu nextInt because of weak randomness in lower bits
-    var rangeSize = end - start;
-    var randomUnder1 = this.nextInt() / this.m;
-    return start + Math.floor(randomUnder1 * rangeSize);
-}
-RNG.prototype.nextIndex = function(array) {
-    return this.nextRange(0, array.length);
-}
+// Run at load
+newgame();
+window.newgame = newgame;
